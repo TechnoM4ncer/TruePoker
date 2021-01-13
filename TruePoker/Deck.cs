ef049db672b941668813e476097a8411978541eb
TechnoM4ncer <kinodekirakashi@live.com>
2021-01-13 13:29:55 -0600

    Create repository with boilerplate code

---
+++ TruePoker/Deck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TruePoker
+{
+    class Deck
+    {
+        public static string[] suit = new string[] { "Diamonds", "Clubs", "Spades", "Hearts" };
+        public static string[] number = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+    }
+}
---
+++ TruePoker/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TruePoker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random seed = new Random();
+            string flopCard1;
+            string flopCard2;
+            string flopCard3;
+            string turnCard;
+            string riverCard;
+
+            flopCard1 = Deck.number[seed.Next(0, 12)] + " of " + Deck.suit[seed.Next(0, 3)];
+            flopCard2 = Deck.number[seed.Next(0, 12)] + " of " + Deck.suit[seed.Next(0, 3)];
+            flopCard3 = Deck.number[seed.Next(0, 12)] + " of " + Deck.suit[seed.Next(0, 3)];
+            turnCard = Deck.number[seed.Next(0, 12)] + " of " + Deck.suit[seed.Next(0, 3)];
+            riverCard = Deck.number[seed.Next(0, 12)] + " of " + Deck.suit[seed.Next(0, 3)];
+
+            Console.WriteLine(flopCard1);
+
+            while (flopCard2 == flopCard1)
+            {
+                flopCard2 = Deck.number[seed.Next(0, 12)] + " of " + Deck.suit[seed.Next(0, 3)];
+            }
+
+            Console.WriteLine(flopCard2);
+
+            while (flopCard3 == flopCard2 || flopCard3 == flopCard1)
+            {
+                flopCard3 = Deck.number[seed.Next(0, 12)] + " of " + Deck.suit[seed.Next(0, 3)];
+            }
+
+            Console.WriteLine(flopCard3);
+
+            while (turnCard == flopCard1 || turnCard == flopCard2 || turnCard == flopCard3)
+            {
+                turnCard = Deck.number[seed.Next(0, 12)] + " of " + Deck.suit[seed.Next(0, 3)];
+            }
+
+            Console.WriteLine(turnCard);
+
+            while (riverCard == flopCard1 || riverCard == flopCard2 || riverCard == flopCard3 || riverCard == turnCard)
+            {
+                riverCard = Deck.number[seed.Next(0, 12)] + " of " + Deck.suit[seed.Next(0, 3)];
+            }
+
+            Console.WriteLine(riverCard);
+
+            Console.ReadKey();
+        }
+    }
+}
